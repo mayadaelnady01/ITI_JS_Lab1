99e16fee3300298a467ef2400a91b5d71c626407
mayadaelnady01 <mayadaelnady01@gmail.com>
2024-12-08 14:08:26 +0200

    intial lab files

---
+++ Script.js
@@ -0,0 +1,19 @@
+// Task 1
+alert("Welcome to my site");
+let Name = prompt("Please enter your name:");
+document.write("Welcome "+Name);
+
+// Task 2
+function Temperature(t) {
+    let message = t >= 30 ? "Hot" : "Cold";
+    console.log(message);
+  }
+
+  Temperature(40);
+  Temperature(12);
+  Temperature(30);
+
+
+
+
+
